d198bc123db4a53b6f750cad20a378b3d1049f2a
Geovane <33160858+GeovaneJorge@users.noreply.github.com>
2025-01-28 18:44:10 -0300

    Add files via upload

---
+++ src/MinhaClasse.java
@@ -0,0 +1,18 @@
+public class MinhaClasse {
+    public static void main(String[] args) {
+        System.out.println("Teste de Classe em Java!");
+
+        String primeiroNome="Geovane";
+        String segundoNome="Sistemas";
+        String nomeCompleto = nomeCompleto (primeiroNome,segundoNome);
+
+        System.out.print(nomeCompleto);
+        
+    }
+
+    public static String nomeCompleto (String primeiroNome, String segundoNome) {
+        return "Resultado do método " + primeiroNome.concat(" ").concat(segundoNome);
+    }
+
+    
+}
