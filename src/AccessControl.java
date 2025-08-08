953eb97cade432dba518d4c304c0d84b6c5457b4
Geovane <33160858+GeovaneJorge@users.noreply.github.com>
2025-08-08 16:05:20 -0300

    Add files via upload
    Desafio 1

---
+++ src/AccessControl.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class Acesso {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        // Entrada de dados do usuário
+        boolean hasPermission = scanner.nextBoolean(); // Lê um valor booleano (true ou false)
+        int age = scanner.nextInt(); // Lê a idade como inteiro
+        
+        if(hasPermission == true && age >= 18){
+            System.out.println("Acesso permitido");
+
+        } else if (hasPermission == false && age >=25 ) {
+            System.out.println("Acesso negado");
+            
+        } else if (hasPermission == true && age < 18) {
+            System.out.println("Acesso insuficiente");
+        }
+
+        scanner.close();
+    }
+}
